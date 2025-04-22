972e9f0f7f8e71982459404956eb15aae99dc330
Andressa Brioschi <andressabrioschi@gmail.com>
2025-04-21 22:14:44 -0300

    ajustes1

--- Main.py
+++ Main.py
@@ -1,31 +1,25 @@
-from flask  import Flask, render_template, request, redirect, flash, url_for
+from flask  import Flask, render_template, request, redirect, flash, url_for, session
 #permissão: pip install flask-sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
-# biblioteca responsável pelo gerenciamneto do login (precisa do pip install flask-login ) 
-from flask_login import LoginManager, login_user, UserMixin, login_required, logout_user, current_user
 
 # a linha abaixo inicia a variavel de aplicação
 #Se não colocar URI não tem como conectar o banco em pg html
 app = Flask(__name__)
 
 #permite o flask_login funcionar corretamente 
-app.secret_key="MedDay"
+app.secret_key="SenhaSuperSecreta"
 
-#Variável que permite o acesso ao flask_login atravéz da variável de aplicação utilizada como parâmetro 
-lm = LoginManager(app)
-
-#faz com que caso o usuário não esteja logado seja direcionado autoamticamente para login
-lm.login_view = '/login'
 
 app.config['SQLALCHEMY_DATABASE_URI']  = \
-    'mysql+pymysql://root:Lucas_62@localhost:3306/projeto_semestral'
+    'mysql+pymysql://root:we123@localhost:3306/projeto_semestral'
 
 #a linha abaixo instancia o banco de dados
 db = SQLAlchemy(app)
 
 
-#UserMixin permite que o flask_login reconhecer a seguinte classe de usuário
-class Cadastro_paciente(UserMixin, db.Model):
+# session['nomeDaSessao'] = request.form['cpf']
+
+class Cadastro_paciente(db.Model):
     nome = db.Column(db.String(200), nullable = False)
     cpf = db.Column(db.String(11), primary_key =True)
     email = db.Column(db.String(200),  nullable=False)
@@ -38,16 +32,9 @@ class Cadastro_paciente(UserMixin, db.Model):
     cidade = db.Column(db.String(200), nullable=True)
     UF = db.Column(db.String(2), nullable=False)
 
-    #O UserMixin por padrão espera que exista um campo chamado id para usar como identificador,
-    #mas já que cpf é o indentificador é necessáio usar a seguinte função para mudar o identificador padrão 
-    def get_id(self):
-        return str(self.cpf)
-
-#variável que carrega o usuário atravez da sua busca do seu cpf
-@lm.user_loader
-def user_loader(cpf):
-    usuario = db.session.query(Cadastro_paciente).filter_by(cpf = cpf).first()
-    return usuario
+@app.route("/login")
+def telaInicial():
+    return render_template("./login.html")
 
 @app.route("/cadastrar")
 def cadastrar_usuario():
@@ -57,18 +44,6 @@ def cadastrar_usuario():
 def resetar_senha():
     return  render_template("./resetarsenha.html")
 
-@app.route("/")
-#faz com que essa rota só possa ser acessada se estiver logado
-@login_required
-def home():
-    return render_template("./home.html", nome = current_user.nome, email = current_user.email)
-
-@app.route('/logout')
-@login_required
-def logout():
-    #realiza o logout do usuário
-    logout_user()
-    return redirect(url_for('home'))
 
 @app.route("/add", methods = ['POST'])
 def add_banco():
@@ -108,16 +83,29 @@ def add_banco():
             alert_txt = ""
             #a linha abaixo adiciona os dados para verificação da entrada de dados
             db.session.add(novo_usuario)
-
-
             #a linha abaixo grava as alterações no banco de dados
             db.session.commit()
+            return redirect ("/login")
 
-    return render_template("./cadastrar.html", alert_value = alert, txt_alert = alert_txt)
-    #a linha abaixo grava as alterações no banco de dados
-    #db.session.commit()
-    #return redirect ("/login")
+ 
     
+@app.route ("/autenticar", methods = ['POST'])
+def autenticar():    
+    cpf = request.form['txtcpf']
+    senha = request.form['txtsenha']
+    #a  linha abaixo é equivalente a um select no banco, onde na clausula where vai o cpf imputado e a senha
+    user = db.session.query(Cadastro_paciente).filter_by(cpf = cpf, senha = senha ).first()
+
+    if user:
+        return render_template("./home.html")
+    else:
+        return render_template("./login.html")
+
+        
+    
+
+
+
     
 
 
@@ -149,22 +137,5 @@ def reset_password():
         alert_txt = 'CPF não válido' 
     return render_template("./resetarsenha.html",alert_value = alert, txt_alert = alert_txt)
 
-@app.route("/login", methods = ['GET','POST'])
-def logar():
-    #se o método da requisição for GET: 
-    if request.method == 'GET':
-        return render_template('./login.html')
-    #se o método da requisição for POST: 
-    elif request.method == 'POST':
-        email_input = request.form['email']
-        senha_input = request.form['senha']
-        user = db.session.query(Cadastro_paciente).filter_by(email = email_input, senha = senha_input).first()
-        if not user:
-            return "Email ou senha incorretos."
-        else:
-            #realiza o login do usuário
-            login_user(user)
-            #redireciona para home depois do login
-            return redirect(url_for('home')) 
 
 app.run(debug=True)
